31adcc65cc83a2914031f7a0cf58b4497f1435ae
blakelizz <oceane.mngs@yahoo.com>
2025-01-22 09:02:28 +0100

    Exercices : Définition de variables. Finit

---
+++ exercices.js
@@ -0,0 +1,14 @@
+// const nom_sorcier = prompt("Comment te nommes-tu, sorcier ? 🧙‍♂️");
+// console.log("Le sorcier se nomme :" + nom_sorcier + " ! 🎉");
+
+const store_name = "La boutique des sorciers";
+let potion_stock = 13;
+let sage_potion = 4;
+
+const store = false;
+if (!store){
+    console.log("La boutique des sociers n'est pas encore ouverte. Revenez plus tard ! ")
+}
+else {
+    console.log("La boutique est ouverte. Tu peux entrer ! ")
+}
